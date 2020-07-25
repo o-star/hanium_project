5a194d2d6c1eb1ba5c0b75a30c980b20560d337e
mook <kcm0147@hanmail.net>
2020-07-25 18:46:38 +0900

    partial html and init environment

--- web/main.js
+++ web/main.js
@@ -1,13 +1,22 @@
 var express=require('express');
 
 var app=express();
-app.set('views','views');
+
+app.set('views','./views');
 app.set('view engine','ejs');
 
 app.use(express.static(__dirname));
 
 app.get('/',function(req,res){
-    res.render('index');
+    res.render('index')
+});
+
+app.get('/inquire',function(req,res){
+    res.render('inquire')
+});
+
+app.get('/realtime',function(req,res){
+    res.render('realtime')
 });
 
 
