66a1c8bbe3f057cf5e3eafb7955ac38339a4c5cd
kcm0147 <kcm0147@hanmail.net>
2020-08-01 23:54:57 +0900

    end inquire page

--- newweb/routes/mysql/ship.js
+++ newweb/routes/mysql/ship.js
@@ -1,14 +1,83 @@
 module.exports = function () {
     var route = require('express').Router();
     var conn = require('../../config/mysql/db')();
+    var bodyParser = require('body-parser');
+
+    route.use(bodyParser.urlencoded({extended : true}));
 
     route.get('/', function (req, res) {
         res.render('index')
     });
 
     route.get('/inquire', function (req, res) {
-        res.render('inquire')
+        var sql = 'SELECT * FROM record';
+        conn.query(sql,function(err,results){
+            if(err){
+                console.log(err);
+                res.status(500).send("DB Error");
+            }
+            res.render('inquire',{results :results});
+        });
+    
     });
+
+    route.post('/inquire',function(req,res){
+        var sql = 'SELECT * FROM record';
+        
+        var shipname=req.body.shipName;
+        var entry=req.body.portEntry;
+        var startdate=req.body.startDate;
+        var starthour=req.body.startHour;
+        var enddate=req.body.endDate;
+        var endhour=req.body.endHour;
+
+        conn.query(sql,function(err,fileds){
+            
+            var results=fileds;
+            
+
+            if(shipname){
+                results=results.filter(function(curRow){
+                    return curRow.ship_name==shipname;
+                })
+            }
+
+            if(entry){
+                results=results.filter(function(curRow){
+                    return curRow.ship_direction==entry;
+                })
+            }
+
+            if(startdate){
+                results=results.filter(function(curRow){
+                    return curRow.date>=startdate;
+                })
+            }
+        
+            if(starthour){
+                results=results.filter(function(curRow){
+                    return curRow.time>=startHour;
+                })
+            }
+
+            if(enddate){
+                results=results.filter(function(curRow){
+                    return curRow.date<=enddate;
+                })
+            }
+
+            if(endhour){
+                results=results.filter(function(curRow){
+                    return curRow.time<=endhour;
+                })
+            }
+
+        
+           res.render('inquire',{results : results});   
+        });
+
+    });
+
 
     route.get('/add', function (req, res) {
         var sql = 'SELECT * FROM temp';
