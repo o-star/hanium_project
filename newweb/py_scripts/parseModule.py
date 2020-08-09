a7105fe01763e3c1c48e330b06c97189d70eb111
nowwater <nowwater0225@gmail.com>
2020-08-10 01:01:50 +0900

    Realtime audio streaming recording

--- newweb/py_scripts/parseModule.py
+++ newweb/py_scripts/parseModule.py
@@ -6,7 +6,6 @@ def rankFunction(texts):
     scores = {'선박명': 0.5, '총톤수는': 0.7, '년': 0.5, '월': 0.5, '일': 0.5, '시': 0.5, '분': 0.5, '울산': 0.5, '예정': 0.5}
     tokenizer = MaxScoreTokenizer(scores=scores)
     keywords = tokenizer.tokenize(texts)
-    print(keywords)
     return keywords
 
 def findDate(keywords):  # 날짜 확인
@@ -73,37 +72,29 @@ def findHarborLocation(keywords):  # 외항/내항 확인
 
 def findShipName(keywords, texts):  # 선박명 확인
     for kwidx in range(len(keywords)):
-        if keywords[kwidx] == "선박명":
-            answerDic['선박명'] = keywords[kwidx + 1]
-
-    #answerDic['선박명'] = keywords[1]
-    # splitText = texts[0].split(' ')
-    # breakPoint = False
-    # for comp in splitText:
-    #     if breakPoint:
-    #         break;
-    #     if '선박명' in comp:
-    #         breakPoint = True;
-    # for name in keywords:
-    #     if comp in name or name in comp:
-    #         answerDic['선박명'] = name
-    #         break
-
+        if "무게" not in keywords[kwidx] and "이름" not in keywords[kwidx]:
+            if "선박명" in answerDic.keys():
+                answerDic['선박명'] += keywords[kwidx]
+            else:
+                answerDic['선박명'] = keywords[kwidx]
+        elif "무게" in keywords[kwidx]:
+            break
 
 def findShipWeight(keywords, texts): #총톤수 확인
+    res = 0
+    temp = ""
     for kwidx in range(len(keywords)):
-        if keywords[kwidx] == "톤" :
-            answerDic['총톤수'] = keywords[kwidx - 1]
-    # splitText = texts[0].split(' ')
-    # splitLen = len(splitText)
-    # for comp in keywords:
-    #     if comp.find('톤') != -1 and comp[0:comp.find('톤') - 1].isdigit():
-    #         answerDic['총톤수'] = comp
-    #         break
-    #     elif comp.isdigit():
-    #         for i in range(0,splitLen):
-    #             if comp == splitText[i] and '톤' in splitText[i + 1]:
-    #                 answerDic['총톤수'] = comp
-    #                 break
+        if "톤" in keywords[kwidx]:
+            if keywords[kwidx][:-1].isdigit():
+                res += int(keywords[kwidx][:-1])
+            temp_idx = kwidx -1
+            while temp_idx >= 0 and keywords[temp_idx].isdigit():
+                res += int(keywords[temp_idx])
+                temp_idx -= 1
+        answerDic['총톤수'] = str(res)
+
 
 #texts = ["선박명 온두리호 총톤수는 육백삼십이톤이며 이천이십년 팔월 십오일 십삼시 오십분에 울산 외항으로 입항할 예정이다"]
+# 이름 현수호 무게 632톤 2020년 8월 1일 9시 15분에 울산 외항으로다가 입항할 예정이다
+#이름은 효동호 무게는 132톤 2020년 8월 9일 23시 17분에 울산 외항으로 출항할 예정이다
+#이름은 타이타닉 로스 포카리 농구 겨드랑이 호 무게는 132톤 2020년 8월 9일 23시 17분에 울산 외항으로 출항할 예정이다
