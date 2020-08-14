a020925a5117aa80bcca3dcab3a24cbe62485ddd
이효동 <good797979@knu.ac.kr>
2020-08-15 01:44:37 +0900

    통계량 부분 구현완료

--- newweb/routes/mysql/ship.js
+++ newweb/routes/mysql/ship.js
@@ -1,11 +1,10 @@
 module.exports = function () {
     var route = require('express').Router();
     var conn = require('../../config/mysql/db')();
- //   var rec = require('../../audio_record/record-to-file');
     var bodyParser = require('body-parser');
-    const {PythonShell} = require("python-shell");
+    const { PythonShell } = require("python-shell");
     const path = require('path');
-    const {response} = require("express");
+    const { response } = require("express");
     const pypath = path.join(__dirname, "../../py_scripts");
     let options = {
         scriptPath: pypath,
@@ -13,27 +12,72 @@ module.exports = function () {
     };
 
 
-    route.use(bodyParser.urlencoded({extended: true}));
+    route.use(bodyParser.urlencoded({ extended: true }));
 
     route.get('/', function (req, res) {
-        res.render('index')
+        var sql = 'SELECT * FROM record ORDER BY date, time';
+        //구해야할것
+        //올해 관제량, 금월 관제량, 금주 관제량, 금일 관제량
+
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = today.getMonth() + 1;
+        const date = today.getDate();
+        const day = today.getDay();
+        const monday = date - day + (day == 0 ? -6 : 1);
+        const sunday = monday + 6;
+
+        let info = [0, 0, 0, 0];
+
+        conn.query(sql, function (err, rows) {
+
+            function sameCheck(curRow) {
+                const dbArray = curRow.date.split('-'); // DB값
+                const dbYear = dbArray[0];
+                const dbMonth = dbArray[1];
+                const dbDate = dbArray[2];
+
+                if (dbYear == year) {
+                    info[0]++;
+                    if (dbMonth == month) {
+                        info[1]++;
+                        if (dbDate == date)
+                            info[3]++;
+                        if (date >= monday && date <= sunday)
+                            info[2]++;
+                    }
+                }
+            }
+            for (i in rows) {
+                sameCheck(rows[i]);
+            }
+            res.render('index', { info: info });
+        });
     });
 
     route.get('/inquire', function (req, res) {
-        var sql = 'SELECT * FROM record';
+
+        var sql = 'SELECT * FROM record ORDER BY date, time';
+
         conn.query(sql, function (err, results) {
             if (err) {
                 console.log(err);
                 res.status(500).send("DB Error");
             }
-            res.render('inquire', {results: results});
+            res.render('inquire', { results: results });
         });
 
     });
 
 
     route.post('/inquire', function (req, res) {
-        var sql = 'SELECT * FROM record';
+        var option = req.body.option;
+        var sql = '';
+
+        if (!option) sql = 'SELECT * FROM record ORDER BY date, time';
+        else if (option == 'time') sql = 'SELECT * FROM record ORDER BY time';
+        else if (option == 'weight_ton') sql = 'SELECT * FROM record ORDER BY weight_ton';
+        else sql = 'SELECT * FROM record ORDER BY ship_name';
 
         var input = {
 
@@ -147,7 +191,7 @@ module.exports = function () {
             if (input.endhour) {
                 results = results.filter(hourSmallerCheck);
             }
-            res.render('inquire', {results: results});
+            res.render('inquire', { results: results });
         });
 
     });
@@ -156,27 +200,34 @@ module.exports = function () {
         var status = req.params.status;
         if (status == 'start') {
             PythonShell.run("realtime_recording.py", options, function (err, data) {
-                if (err) throw err;
+                if (err) {
+                    console.log('여기서 에러터짐!!');
+                    throw err;
+                }
                 //res.status(200).json({data: JSON.parse(data), success: true});
-                var value = data[1].split(' ') //data[1].split(' ');
-                console.log(value);
-                var sql =
-                    `
+                else {
+                    var value = data[0].split(' ');
+
+                    console.log('음성 데이터 들어온 것' + value);
+                    console.log(value);
+
+                    var sql =
+                        `
                 INSERT INTO temp (ship_name, weight_ton, ship_direction, port_position, date, time)
                 VALUES ("${value[0]}", ${value[1]}, "${value[2]}", "${value[3]}", "${value[4]}", "${value[5]}");
                 `;
 
-                console.log(value);
-                conn.query(sql, function (err, result) {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).send('Internal Server Error');
-                    } else {
-                        console.log("Number of records inserted: " + result.rowsAffected);
-                        res.redirect('/add');
-                    }
-                });
-        });
+                    console.log(value);
+                    conn.query(sql, function (err, result) {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).send('Internal Server Error');
+                        } else {
+                            res.redirect('/add');
+                        }
+                    });
+                }
+            });
         }
     })
 
@@ -187,19 +238,19 @@ module.exports = function () {
                 console.log(err);
                 res.status(500).send('Internal Server Error');
             }
-            res.render('add', {records: records});
+            res.render('add', { records: records });
         });
     });
 
     route.get('/add/:id', function (req, res) {
         var id = req.params.id;
         var sql =
-                `
+            `
     INSERT INTO record (ship_name, weight_ton, ship_direction, port_position, date, time)
     SELECT ship_name, weight_ton, ship_direction, port_position, date, time
     FROM temp
     WHERE id = ?`
-        ;
+            ;
 
         conn.query(sql, [id], function (err, rows, fields) {
             if (err) {
