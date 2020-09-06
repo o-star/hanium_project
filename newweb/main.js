dfe1ebf6f4e1cf050f994d24c17160f6bc64fe00
root <root@ip-172-31-63-238.ap-northeast-2.compute.internal>
2020-09-06 06:25:41 +0000

    http https redirect

--- newweb/main.js
+++ newweb/main.js
@@ -5,16 +5,24 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 
-const options = { // letsencrypt로 받은 인증서 경로를 입력해 줍니다.
+const options = {
 
     ca: fs.readFileSync('/etc/letsencrypt/live/www.9shipcontrol.com/fullchain.pem'),
     key: fs.readFileSync('/etc/letsencrypt/live/www.9shipcontrol.com/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/www.9shipcontrol.com/cert.pem')
     
     };
-    
-    http.createServer(app).listen(80);
-    https.createServer(options, app).listen(443)
+   
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
+
+app.use(function(req,res,next){
+	if(!req.secure){
+		res.redirect("https://"+"9shipcontrol.com"+req.url);
+	}else{
+		next();
+	}
+});
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
