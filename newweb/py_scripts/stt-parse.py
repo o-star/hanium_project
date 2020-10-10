a56da34936d33561144cecd98a2ac05306e9e8bf
nowwater <nowwater0225@gmail.com>
2020-10-10 16:28:32 +0900

    Sound Adaptation update for transcribing accuracy.

--- newweb/py_scripts/stt-parse.py
+++ newweb/py_scripts/stt-parse.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from google.cloud import speech_v1
+from google.cloud import speech_v1p1beta1
 
-from google.cloud.speech_v1 import enums
+from google.cloud.speech_v1p1beta1 import enums
+from google.cloud.speech_v1p1beta1 import types
 
 import io
 import os
@@ -15,18 +16,41 @@ def sample_recognize():
     Args:
       local_file_path Path to local audio file, e.g. /path/audio.wav
     """
-    client = speech_v1.SpeechClient()
+    client = speech_v1p1beta1.SpeechClient()
     # local_file_path = 'resources/brooklyn_bridge.raw'
     # The language of the supplied audio
 
     language_code = "ko_KR"
     # Sample rate in Hertz of the audio data sent
 
-    sample_rate_hertz = 48000 #16000 #44100 #16000
+    # Chrome : 48000 Hz
+    # Safari : 16000 Hz
+    # Firefox : 44100 Hz
+    sample_rate_hertz = 48000
+
     # Encoding of audio data sent. This sample sets this explicitly.
     # This field is optional for FLAC and WAV audio formats.
 
-    speech_contexts = [{"phrases" : "선박명","boost" : 20.0},{"phrases" : "창묵호","boost" : 20.0}]
+    speech_contexts = [
+        {"phrases" : "울산", "boost" : 20.0},
+        {"phrases" : "묵호", "boost" : 20.0},
+        {"phrases" : "선박명", "boost" : 20.0},
+        {"phrases" : "외항", "boost" : 20.0},
+        {"phrases" : "내항", "boost" : 20.0},
+        {"phrases" : "출항", "boost" : 20.0},
+        {"phrases" : "입항", "boost" : 20.0},
+        {"phrases" : "무게는", "boost" : 20.0},
+        {"phrases" : "톤수", "boost" : 20.0},
+        {"phrases" : "톤", "boost" : 20.0},
+        {"phrases" : "총톤수", "boost" : 20.0},
+        {"phrases" : "내항", "boost" : 20.0},
+        {"phrases" : "년", "boost" : 20.0},
+        {"phrases" : "월", "boost" : 20.0},
+        {"phrases" : "일", "boost" : 20.0},
+        {"phrases" : "시", "boost" : 20.0},
+        {"phrases" : "분", "boost" : 20.0},
+        {"phrases" : "초", "boost" : 20.0}
+    ]
     encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
     config = {
         "speech_contexts" : speech_contexts,
@@ -37,7 +61,7 @@ def sample_recognize():
     file_name = os.path.join(
         os.getcwd(),
         'audio_record',
-        "result.wav" #sys.argv[7]#'wyhg.wav' #sys.argv[7] #'fzac.wav'#sys.argv[7] nhuy.wav
+        "result.wav"
     )
 
     with io.open(file_name, "rb") as audio_file:
@@ -51,11 +75,7 @@ def sample_recognize():
         f.write(texts)
 
     keywords = pm.rankFunction(texts)
-    #texts = "선박명 현수호 총톤수는 632 톤 2020년 8월 1일 9시 15분에 울산 외항으로 입항할 예정이다"
-    #texts = [pm.normalize(text, english=False, number=True) for text in texts]
-    # texts 전처리 영어 미포함, 숫자 미포함 설정
 
-    #print(keywords)
     pm.findInOut(keywords)  # 입/출항 추출 함수
     pm.findHarborLocation(keywords)  # 외/내항 추출 함수
     pm.findDate(keywords)  # 날짜 데이터 추출함수
@@ -63,13 +83,9 @@ def sample_recognize():
     pm.findShipName(keywords, texts)  # 선박명 추출함수
     pm.findShipWeight(keywords, texts)  # 총톤수 추출함수
 
-    #print(pm.answerDic)
-    #print(pm.answerDic['선박명'], pm.answerDic['입/출항'], pm.answerDic['날짜'], pm.answerDic['시간'],
-    #      pm.answerDic['외/내항'], pm.answerDic['총톤수'])
+    # 선박명, 입/출항, 날짜, 시간, 외/내항, 총톤수
     print(pm.answerDic[sys.argv[1]], pm.answerDic[sys.argv[2]], pm.answerDic[sys.argv[3]], pm.answerDic[sys.argv[4]], pm.answerDic[sys.argv[5]], pm.answerDic[sys.argv[6]])
 
 sample_recognize()
 
-#"선박명 정석호 총톤수는 삼백삼십이톤이며 이천이십년 팔월 칠일 십구시 오십분에 울산 외항으로 출항할 예정이다"
-#선박명 현수호 총톤수는 632톤 2020년 8월 1일 9시 15분에 울산 외항으로 입항할 예정이다
-#총톤수는 58톤 선박명 창묵호 2020년 8월 9일 18시 28분에 울산 내항으로 출항할 예정이다
+# 입력 예시 : 선박명 현수호 총톤수는 632톤 2020년 8월 1일 9시 15분에 울산 외항으로 입항할 예정이다
